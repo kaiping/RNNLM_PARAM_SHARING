28224d8f9ce9c6a79c301250a59ce2d862111745
kaiping <kaiping@comp.nus.edu.sg>
2015-07-26 19:42:14 +0800

    Fix some bugs in /examples/rnnlm/create_shard.cc

--- examples/rnnlm/create_shard.cc
+++ examples/rnnlm/create_shard.cc
@@ -20,7 +20,7 @@
 
 using singa::DataShard;
 
-using StrIntGreaterMap =  std::map<std::string, int, std::greater<std::string> >;
+using StrIntGreaterMap = std::map<std::string, int, std::greater<std::string> >;
 
 
 void create_shard(const char *input, const char *classOutput, const char *wordOutput, int nclass) {
@@ -37,32 +37,35 @@ void create_shard(const char *input, const char *classOutput, const char *wordOu
     }
     nwords = wordFreqMap.size();
 
+    int sumFreq = 0;
+    for (auto it : wordFreqMap) {
+        sumFreq += it->second;
+    }
+
     // index words after sorting, and split words into classes by freq
     std::vector<std::pair<int, int> > classInfo;
     StrIntGreaterMap wordIdxMap, wordClassIdxMap;
     int classIdxCnt = 0;
-    int tmpWordCnt = 0;
-    int lastStartPos = 0;
-    int curWordIdx = 0;
+    int tmpWordFreqSum = 0;
+    int nextStartPos = 0;
+    int wordIdxCnt = 0;
     for (auto it : wordFreqMap) {
         // index words
-        if (wordIdxMap.end() == wordIdxMap.find(it->first)) {
-            wordIdxMap[it->first] = wordIdxMap.size();
-        }
+        wordIdxMap[it->first] = wordIdxMap.size();
 
         // generate classes
-        tmpWordCnt += it->second;
+        tmpWordFreqSum += it->second;
         wordClassIdxMap[it->first] = classIdxCnt;
 
         // split a new class
         // ensure no empty class
-        if ((tmpWordCnt > (classIdxCnt + 1) * nwords / nclass) || (nwords - curWordIdx) <= nclass - classIdxCnt) {
-            classInfo.emplace_back(std::make_pair(lastStartPos, curWordIdx));
-            lastStartPos = curWordIdx + 1;
+        if ((tmpWordFreqSum >= (classIdxCnt + 1) * sumFreq / nclass) || (nwords - wordIdxCnt) <= nclass - classIdxCnt) {
+            classInfo.emplace_back(std::make_pair(nextStartPos, wordIdxCnt));
+            nextStartPos = wordIdxCnt + 1;
             ++classIdxCnt;
         }
 
-        ++curWordIdx;
+        ++wordIdxCnt;
     }
 
     // generate class data
@@ -71,7 +74,7 @@ void create_shard(const char *input, const char *classOutput, const char *wordOu
     DataShard classShard(classOutput, DataShard::kCreate);
     singa::Record record;
     record.set_type(singa::Record::kWordClass);
-    singa::WordClassRecord *classRecord = record.mutable_class();
+    singa::WordClassRecord *classRecord = record.mutable_class_record();
     for (int i = 0; i != classInfo.size(); ++i) {
         classRecord->set_start(classInfo[i].first);
         classRecord->set_end(classInfo[i].second);
@@ -80,15 +83,15 @@ void create_shard(const char *input, const char *classOutput, const char *wordOu
         classShard.Insert(std::string(key), record);
     }
     classShard.Flush();
-    record.clear_class();
+    record.clear_class_record();
 
     // generate word data
     // reset input stream status for second loading
     in.clear();
     in.seekg(0, std::ios_base::beg);
     DataShard wordShard(wordOutput, DataShard::kCreate);
-    record.set_type(singa::Record::kWordClass);
-    singa::SingleWordRecord *wordRecord = record.mutable_word();
+    record.set_type(singa::Record::kSingleWord);
+    singa::SingleWordRecord *wordRecord = record.mutable_word_record();
     int wordStreamCnt = 0;
     while (in >> word) {
         // TODO (kaiping): do not forget here if modify tokenize logic
